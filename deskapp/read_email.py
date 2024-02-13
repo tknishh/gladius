446edbcc211cb7d9bd9dd7bc157b3552856ad7c5
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2024-02-13 18:08:56 +0530

    read_email

---
+++ deskapp/read_email.py
@@ -0,0 +1,20 @@
+from simplegmail import Gmail
+from simplegmail.query import construct_query
+
+# Create a Gmail object
+gmail = Gmail()
+
+query_params = {
+    "newer_than": (2, "day")    
+}
+
+messages = gmail.get_messages(query=construct_query(query_params))
+
+for message in messages:
+    print("To: " + message.recipient)
+    print("From: " + message.sender)
+    print("Subject: " + message.subject)
+    print("Date: " + message.date)
+    print("Preview: " + message.snippet)
+    
+    # print("Message Body: " + message.html)
